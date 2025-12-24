64d5703d52e0ebe34c6f1e313e3fee842b80d6d1
ThankGod George <thankgod@flutterwavego.com>
2025-12-24 19:43:00 +0100

    fix cors issue

--- src/main.ts
+++ src/main.ts
@@ -26,14 +26,9 @@ async function bootstrap() {
   );
   app.use(compression());
 
-  // Enable CORS
-  const corsOrigin =
-    process.env.NODE_ENV === 'production'
-      ? configService.get<string>('ALLOWED_ORIGINS')?.split(',') || []
-      : true; // Allow all origins in development
-
+  // Enable CORS - Allow access from anywhere
   app.enableCors({
-    origin: corsOrigin,
+    origin: true,
     credentials: true,
   });
 
